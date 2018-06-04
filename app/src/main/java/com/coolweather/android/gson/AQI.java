2f9080d7ecab3470d98ac39f849b307a286842af
Lucas <1912988332@qq.com>
2018-06-04 12:24:21 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2018/6/4/004.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
